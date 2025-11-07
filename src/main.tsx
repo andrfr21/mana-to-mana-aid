e7d60aa5d965ac999ee6eb24496830c913845896
Francois Andreani <andreanfrancois@gmail.com>
2025-11-07 18:38:22 +0100

    Import CSS file for main component styling
    Added CSS import for styling.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import "./index.css"; // ou "./globals.css" selon ton projet
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
