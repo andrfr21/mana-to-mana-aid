a2666beca9f7ad6c7225659f161c6d5fe6409756
Francois Andreani <andreanfrancois@gmail.com>
2025-11-07 17:02:18 +0100

    Refactor routing to use createBrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.tsx";
-import Beneficiary from "./pages/Beneficiary.tsx";
-import Donor from "./pages/Donor.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
+const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+
+const rootEl = document.getElementById("root");
+if (!rootEl) throw new Error("#root introuvable dans index.html");
+
+createRoot(rootEl).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/beneficiary" element={<Beneficiary />} />
-        <Route path="/donor" element={<Donor />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
